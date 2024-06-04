d725d48f7b577398e89d40b5fff3468ec836b2dc
laurenCho9 <lauren.choux@gmail.com>
2024-06-04 15:46:53 +0900

    HOTFIX: 다시 버셀로 배포해보기

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -32,7 +32,7 @@ function AppLayout() {
               navbarScroll
             >
               <Nav.Link href="/">Home</Nav.Link>
-              {/* <Nav.Link href="/movies">Movies</Nav.Link> */}
+              <Nav.Link href="/movies">Movies</Nav.Link>
             </Nav>
             <Form className="d-flex" onSubmit={searchByKeyword}>
               <Form.Control
